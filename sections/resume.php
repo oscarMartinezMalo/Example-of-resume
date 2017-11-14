7890ffc9f64e543fd91793cb6e4d30a198b5810a
Oscar Home <ommalor@gmail.com>
2017-11-13 23:36:33 -0500

    Adding dots resume section
    Working in resume section

--- sections/resume.php
+++ sections/resume.php
@@ -1,10 +1,13 @@
 <section class="resume container">
 	<h1>Resume</h1>
 	<hr>
-	<h4 class="text-center m-5">Experience</h4>
+	<h4 class="text-center mb-5"><span>Experience</span></h4>
 	<div class="row">
 		<div class="col-sm-6">
+			<div class="vl"></div>
 			<span class="fa fa-folder-open"></span>
+			<div class="dot dot-first"></div>
+			<div class="dot dot-second"></div>
 			<h5>Senior Developer</h5>
 			<p>Company Name 2013-Current</p>
 			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
@@ -15,6 +18,9 @@
 			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
 		</div>
 		<div class="col-sm-6">
+			<div class="dot dot-first"></div>
+			<div class="dot dot-second"></div>
+			<div class="vl"></div>
 			<h5>Senior Developer</h5>
 			<p>Company Name 2013-Current</p>
 			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
@@ -25,9 +31,10 @@
 			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
 		</div>
 	</div>
-	<h4 class="text-center m-5">Education</h4>
+	<h4 class="text-center mb-5"><span>Education</span></h4>
 	<div class="row">
 		<div class="col-sm-6">
+			<div class="vl"></div>
 			<span class="fa fa-book"></span>
 			<h5>Senior Developer</h5>
 			<p>Company Name 2013-Current</p>
@@ -39,6 +46,7 @@
 			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
 		</div>
 		<div class="col-sm-6">
+			<div class="vl"></div>
 			<h5>Senior Developer</h5>
 			<p>Company Name 2013-Current</p>
 			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
