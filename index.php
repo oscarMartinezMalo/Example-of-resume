5a17388c80294e62bab624802d6d04520a793c94
Oscar <ommalor@gmail.com>
2017-11-15 16:55:12 -0500

    milestone
    Milestone already done

--- index.php
+++ index.php
@@ -14,9 +14,10 @@
 		<?php include "sections/navbar.php"; ?>  
 	</header>
 		<?php include "sections/heroImage.php"; ?> 
-		<div class="container">
+		<div>
 			<?php include "sections/aboutMe.php"; ?>			
 			<?php include "sections/resume.php"; ?>		
+			<?php include "sections/milestonesAchived.php" ;?>	
 		</div>
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
--- sections/resume.php
+++ sections/resume.php
@@ -1,75 +1,78 @@
-<section class="resume container">
-	<h1>Resume</h1>
-	<hr>
-	<div class="row">
-		<div class="col-12">
-			<div class="vl d-none"></div>
-			<h4 class="mb-5"><span class="d-md-inline d-block">Experience</span></h4>
+<div class="resume container-fluid">
+	<section class="container">
+		<h1>Resume</h1>
+		<hr>
+		<div class="row ">
+			<div class="col-12">
+				<div class="vl d-none d-sm-block"></div>
+				<h4 class="pb-5"><span class="d-md-inline d-block">Experience</span></h4>
+			</div>
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-sm-6 text-sm-right text-center">
-			<div class="vl d-none d-sm-block"></div>
-			<span class="fa fa-folder-open d-none d-sm-block"></span>
-			<div class="dot dot-first d-none d-sm-block"></div>
-			<div class="dot dot-second d-none d-sm-block"></div>
-			<h5>Senior Developer</h5>
-			<p>Company Name 2013-Current</p>
-			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
+		<div class="row">
+			<div class="col-sm-6 text-sm-right text-center">
+				<div class="vl d-none d-sm-block"></div>
+				<span class="fa fa-folder-open d-none d-sm-block"></span>
+				<div class="dot dot-first d-none d-sm-block"></div>
+				<div class="dot dot-second d-none d-sm-block"></div>
+				<h5>Senior Developer</h5>
+				<p>Company Name 2013-Current</p>
+				<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
+			</div>
+			<div class="col-sm-6 text-sm-left text-center">
+				<h5>Senior Developer1</h5>
+				<p>Company Name 2013-Current</p>
+				<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
+			</div>
+			<div class="col-sm-6 text-sm-right text-center">
+				<div class="dot dot-first d-none d-sm-block"></div>
+				<div class="dot dot-second d-none d-sm-block"></div>
+				<div class="vl d-none d-sm-block"></div>
+				<h5>Senior Developer</h5>
+				<p>Company Name 2013-Current</p>
+				<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
+			</div>
+			<div class="col-sm-6 text-sm-left text-center">
+				<h5>Senior Developer1</h5>
+				<p>Company Name 2013-Current</p>
+				<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
+			</div>
 		</div>
-		<div class="col-sm-6 text-sm-left text-center">
-			<h5>Senior Developer1</h5>
-			<p>Company Name 2013-Current</p>
-			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
+		<div class="row ">
+			<div class="col-12">
+				<div class="vl d-none d-sm-block"></div>
+				<h4 class="mb-5"><span class="d-md-inline d-block">Education</span></h4>
+			</div>
 		</div>
-		<div class="col-sm-6 text-sm-right text-center">
-			<div class="dot dot-first d-none d-sm-block"></div>
-			<div class="dot dot-second d-none d-sm-block"></div>
-			<div class="vl d-none d-sm-block"></div>
-			<h5>Senior Developer</h5>
-			<p>Company Name 2013-Current</p>
-			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
+		<div class="row">
+			<div class="col-sm-6 text-sm-right text-center">
+				<div class="vl d-none d-sm-block"></div>
+				<span class="fa fa-book d-none d-sm-block"></span>
+				<div class="dot dot-first d-none d-sm-block"></div>
+				<div class="dot dot-second d-none d-sm-block"></div>
+				<h5>Senior Developer</h5>
+				<p>Company Name 2013-Current</p>
+				<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
+			</div>
+			<div class="col-sm-6 text-sm-left text-center">
+				<h5>Senior Developer1</h5>
+				<p>Company Name 2013-Current</p>
+				<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
+			</div>
+			<div class="col-sm-6 text-sm-right text-center pb-0 pb-sm-5">
+				<div class="vl d-none d-sm-block"></div>
+				<div class="dot dot-first d-none d-sm-block"></div>
+				<div class="dot dot-second d-none d-sm-block"></div>
+				<h5>Senior Developer</h5>
+				<p>Company Name 2013-Current</p>
+				<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
+			</div>
+			<div class="col-sm-6 text-sm-left text-center">
+				<h5>Senior Developer1</h5>
+				<p>Company Name 2013-Current</p>
+				<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
+			</div>
+			<span class="fa fa-bookmark d-none d-sm-block"></span
 		</div>
-		<div class="col-sm-6 text-sm-left text-center">
-			<h5>Senior Developer1</h5>
-			<p>Company Name 2013-Current</p>
-			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
-		</div>
-	</div>
-	<div class="row ">
-		<div class="col-12">
-			<div class="vl d-none d-sm-block"></div>
-			<h4 class="mb-5"><span class="d-md-inline d-block">Education</span></h4>
-		</div>
-	</div>
-	<div class="row" style="margin-bottom: 20px;">
-		<div class="col-sm-6 text-sm-right text-center">
-			<div class="vl d-none d-sm-block"></div>
-			<span class="fa fa-book d-none d-sm-block"></span>
-			<div class="dot dot-first d-none d-sm-block"></div>
-			<div class="dot dot-second d-none d-sm-block"></div>
-			<h5>Senior Developer</h5>
-			<p>Company Name 2013-Current</p>
-			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
-		</div>
-		<div class="col-sm-6 text-sm-left text-center">
-			<h5>Senior Developer1</h5>
-			<p>Company Name 2013-Current</p>
-			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
-		</div>
-		<div class="col-sm-6 text-sm-right text-center">
-			<div class="vl d-none d-sm-block"></div>
-			<div class="dot dot-first d-none d-sm-block"></div>
-			<div class="dot dot-second d-none d-sm-block"></div>
-			<h5>Senior Developer</h5>
-			<p>Company Name 2013-Current</p>
-			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
-		</div>
-		<div class="col-sm-6 text-sm-left text-center">
-			<h5>Senior Developer1</h5>
-			<p>Company Name 2013-Current</p>
-			<p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, voluptates, odit similique tempore voluptas, ratione omnis ab, cumque distinctio provident error cum. Quia, laudantium, id! Commodi rem, libero dolorum ipsum!</p>
-		</div>
-	</div>
 
-</section>
+	</section>
+</div>
