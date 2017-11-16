4ef6a57d65e6411bc9bf5c7e5b5cc5c953cc031e
Oscar <ommalor@gmail.com>
2017-11-16 16:53:16 -0500

    myservices
    steel working in icons css

--- sections/heroImage.php
+++ sections/heroImage.php
@@ -1,7 +1,7 @@
 <div class="hero-image">
 	<div class="hero-text">
 		<h1>Walter Izquierdo</h1>
-		<p class="mb-0">Php. Data Mining</p>
+		<p class="pb-3">Php. Data Mining</p>
 		<a class="fa fa-twitter" target="_blank" aria-hidden="true"></a>
 		<a class="fa fa-youtube" target="_blank" aria-hidden="true"></a>
 		<a class="fa fa-linkedin" target="_blank" aria-hidden="true"></a>
@@ -10,5 +10,4 @@
 	<div class="image-wrap">
 		<img src="./images/faceImage.jpg" class="mx-auto d-block img-thumbnail" alt="">
 	</div>
-
 </div>
--- sections/myservices.php
+++ sections/myservices.php
@@ -4,40 +4,75 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-6 col-md-5">
-				<h5>WebDesign</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				<div class="square m-auto rounded float-right"><span class="fa fa-coffee center-icons"></span></div>
+				<div class="row">
+					<div class="col-12 col-md-8 col-lg-9 text-right">
+						<h5>WebDesign</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3">
+							<div class="square m-auto rounded float-right"><span class="fa fa-coffee center-icons"></span></div>
+					</div>
+				</div>
 			</div>
 			<div class="col-6 col-md-5 offset-0 offset-md-2">
-				<h5>WebDesign</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				<div class="square  m-auto"><span class="fa fa-coffee center-icons"></span></div>
+				<div class="row d-flex">
+					<div class="col-md-8 text-left order-md-last">
+						<h5>WebDesign</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+					<div class="col-md-4 order-md-first">
+							<div class="square m-auto rounded float-left float-md-right"><span class="fa fa-coffee center-icons"></span></div>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-6 col-md-5">
-				<h5>WebDesign</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				<div class="square  m-auto"><span class="fa fa-coffee center-icons"></span></div>
+				<div class="row">
+					<div class="col-12 col-md-8 col-lg-9 text-right">
+						<h5>WebDesign</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3">
+							<div class="square m-auto rounded float-right"><span class="fa fa-coffee center-icons"></span></div>
+					</div>
+				</div>
 			</div>
 			<div class="col-6 col-md-5 offset-0 offset-md-2">
-				<h5>WebDesign</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				<div class="square  m-auto"><span class="fa fa-coffee center-icons"></span></div>
+				<div class="row d-flex">
+					<div class="col-md-8 push-md-4 text-left order-md-last">
+						<h5>WebDesign</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+					<div class="col-md-4 pull-md-8 order-md-first">
+							<div class="square m-auto rounded float-left float-md-right"><span class="fa fa-coffee center-icons"></span></div>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-6 col-md-5">
-				<h5>WebDesign</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				<div class="square  m-auto"><span class="fa fa-coffee center-icons"></span></div>
+				<div class="row">
+					<div class="col-12 col-md-8 col-lg-9 text-right">
+						<h5>WebDesign</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3">
+							<div class="square m-auto rounded float-right"><span class="fa fa-coffee center-icons"></span></div>
+					</div>
+				</div>
 			</div>
 			<div class="col-6 col-md-5 offset-0 offset-md-2">
-				<h5>WebDesign</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				<div class="square  m-auto"><span class="fa fa-coffee center-icons"></span></div>
+				<div class="row d-flex">
+					<div class="col-md-8 push-md-4 text-left order-md-last">
+						<h5>WebDesign</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+					<div class="col-md-4 pull-md-8 order-md-first">
+							<div class="square m-auto rounded float-left float-md-right"><span class="fa fa-coffee center-icons"></span></div>
+					</div>
+				</div>
 			</div>
-		</div>
 		</div>
 	</div>
 </div>
