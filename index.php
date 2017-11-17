549958f803b67b0f5eadd6c155012232912b4974
Oscar <ommalor@gmail.com>
2017-11-17 12:25:29 -0500

    My services done
    my services done , now working in skill progress bar circle

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
 			<?php include "sections/resume.php"; ?>		
 			<?php include "sections/milestonesAchived.php" ;?>	
 			<?php include "sections/myservices.php" ;?>	
+			<?php include "sections/skills.php" ;?>	
 		</div>
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
---
+++ sections/skills.php
@@ -0,0 +1,7 @@
+<div class="skills container-fluid">
+	<div class="container">
+		<h1>Skills</h1>
+		<hr>
+
+	</div>
+</div>
