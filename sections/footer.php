40be6b76fb7b7e0e0738b89602800e7bd50faac3
Oscar <ommalor@gmail.com>
2017-12-11 17:05:31 -0500

    Optimizations
    Optimization pagespeed

--- sections/footer.php
+++ sections/footer.php
@@ -1,4 +1,1 @@
-<footer class="container text-center">
-  <a href="#myPage" class="fa fa-chevron-circle-up" title="To Top"></a>
-  <p>You made it to the bottom<a href="https://www.walter.com " title="Visit Walter Izquierdo"> www.walter.com</a></p> 
-</footer>
+<footer class="container text-center"><a href="#myPage" class="fa fa-chevron-circle-up" title="To Top"></a><p>You made it to the bottom<a href="https://www.walter.com " title="Visit Walter Izquierdo"> www.walter.com</a></p> </footer>
